713d4be1be1a7263cbeaffdbfd31493613cfb7cd
Yochem van Rosmalen <yochem+git@icloud.com>
2021-09-13 17:26:56 +0200

    New checkboxes

--- public/js/select.js
+++ public/js/select.js
@@ -1,25 +1,27 @@
-var checkboxen = [];
+let artList = document.getElementById('artworks');
 
-for (var i = 0; i < voorInstellingen.length; i++) {
-  document.querySelector('ul[id=namen]').innerHTML += '<li class=naam id="'+i+'">'+ voorInstellingen[i].Name + '</li>';
-  document.querySelector('ul[id=checkboxen]').innerHTML += '<li> <input class="check" id="'+i+'"type="checkbox"> </li>';
+for (let artwork of artworks) {
+    let li = document.createElement('li');
+    li.innerHTML = `<label>
+            <input type="checkbox" name="${artwork.Name}"
+            ${artwork.InRandom ? 'checked' : ''} value="value">
+            ${artwork.Name}
+        </label>`;
+    li.addEventListener('mouseover', toonVoorbeeld);
+    li.addEventListener('click', toonVoorbeeld);
+    artList.appendChild(li);
 }
 
-var lijstVanNamen = document.querySelectorAll('li[class=naam]');
-
-var lijstCheckboxen = document.querySelectorAll('input[class=check]');
-
-for (var i = 0; i < lijstVanNamen.length; i++) {
-  lijstVanNamen[i].addEventListener('mouseover',toonVoorbeeld);
-  lijstVanNamen[i].addEventListener('click',toonVoorbeeld);
-  lijstCheckboxen[i].addEventListener('mouseover',toonVoorbeeld);
-  lijstCheckboxen[i].addEventListener('click',toonVoorbeeld);
-}
-
-function toonVoorbeeld(){
-  this.style.cursor = 'default';
-  for (var i = 0; i < voorInstellingen[this.id].TL.length; i++) {
-    if (voorInstellingen[this.id].TL[i] == true) {
+function toonVoorbeeld() {
+  let selectedArtwork = this.querySelector('input').name;
+  for (let artwork of artworks) {
+      if (artwork.Name == selectedArtwork) {
+          var TLsetup = artwork.TL;
+          break;
+      }
+  }
+  for (const [i, tlOn] of TLsetup.entries()) {
+    if (tlOn) {
       document.querySelector('div[id=T'+i+']').classList.replace('off2', 'on');
     } else {
       document.querySelector('div[id=T'+i+']').classList.replace('on', 'off2');
@@ -27,20 +29,17 @@ function toonVoorbeeld(){
   }
 }
 
-
-
-document.querySelector('button[id=createNew]').addEventListener('mouseover', mouseOverKnop1);
-document.querySelector('button[id=createNew]').addEventListener('mouseout', mouseOutKnop1);
-document.querySelector('button[id=createNew]').addEventListener('mouseover', pointerMuis);
-
-function mouseOverKnop1(){
-  this.style.background = '#D6D6D6';
-}
-
-function mouseOutKnop1(){
-  this.style.background = '#FFFFFF';
-}
-
-function pointerMuis(){
-  this.style.cursor = 'pointer';
+function sendCheckboxClick() {
+  let selectedArtwork = this.querySelector('input').name;
+  for (let artwork of artworks) {
+      if (artwork.Name == selectedArtwork) {
+          var TLsetup = artwork.TL;
+          break;
+      }
+  }
+    if (this.checked) {
+        alert("i am checked");
+    } else {
+        alert("i am not checked")
+    }
 }
